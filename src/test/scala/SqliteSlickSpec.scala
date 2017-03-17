6c8453ebfd7a2b6c0ed3f5ac633c5230980b0ea3
Marc A. Saegesser <marc@saegesser.org>
2017-03-17 12:12:42 -0500

    Remove extraneous tests.

--- src/test/scala/SqliteSlickSpec.scala
+++ src/test/scala/SqliteSlickSpec.scala
@@ -30,42 +30,14 @@ class SqliteSlickSpec extends AsyncWordSpec with Matchers with StrictLogging {
 
 
   "TestData" should {
-    "add several records" in { fixture =>
-      val data = List.fill(900)((Random.nextString(10), Random.nextInt(), Random.nextString(10)))
-
-      ( for {
-        _  <- fixture.create
-        _  <- Future.sequence(data.map((fixture.addRecord _).tupled))
-        rs <- fixture.getAll()
-      } yield rs
-      ) map { rs =>
-        rs shouldEqual data
-      }
-    }
-
-    "add and modify several records" in { fixture =>
-      val data = List.fill(500)((Random.nextString(10), Random.nextInt(), Random.nextString(10)))
-      val updates = data.map { case (c1, c2, c3) => (c1, c3.take(5)) }
-
-      ( for {
-        _  <- fixture.create
-        _  <- Future.sequence(data.map((fixture.addRecord _).tupled))
-        _  <- Future.sequence(updates.map((fixture.updateC3 _).tupled))
-        rs <- fixture.getAll()
-      } yield rs
-      ) map { rs =>
-        rs shouldEqual (data map { d => (d._1, d._2, d._3.take(5)) })
-      }
-    }
-
     "add and modify records in a for comprehension transaction" in { fixture =>
-      val data = List.fill(500)((Random.nextString(10), Random.nextInt(), Random.nextString(10)))
+      val data = List.fill(50)((Random.nextString(10), Random.nextInt(), Random.nextString(10)))
       val updates = data.map { case (c1, c2, c3) => (c1, c2*2, c3.take(5)) }
 
       ( for {
         _  <- fixture.create
         _  <- Future.sequence(data.map((fixture.addRecord _).tupled))
-        _  <- Future.sequence(updates.map((fixture.updateC2C3 _).tupled))
+        _  <- Future.sequence(updates.map((fixture.updateC2C3a _).tupled))
         rs <- fixture.getAll()
       } yield rs
       ) map { rs =>
@@ -74,13 +46,13 @@ class SqliteSlickSpec extends AsyncWordSpec with Matchers with StrictLogging {
     }
 
     "add and modify records in a transaction" in { fixture =>
-      val data = List.fill(500)((Random.nextString(10), Random.nextInt(), Random.nextString(10)))
+      val data = List.fill(50)((Random.nextString(10), Random.nextInt(), Random.nextString(10)))
       val updates = data.map { case (c1, c2, c3) => (c1, c2*2, c3.take(5)) }
 
       ( for {
         _  <- fixture.create
         _  <- Future.sequence(data.map((fixture.addRecord _).tupled))
-        _  <- Future.sequence(updates.map((fixture.updateC2C3X _).tupled))
+        _  <- Future.sequence(updates.map((fixture.updateC2C3b _).tupled))
         rs <- fixture.getAll()
       } yield rs
       ) map { rs =>
@@ -112,7 +84,7 @@ class TestData(dbFile: Path) {
   def updateC3(c1: String, c3: String): Future[Int] =
     db.run(testTable.filter(_.c1 === c1).map(_.c3).update(c3))
 
-  def updateC2C3(c1: String, c2: Int, c3: String): Future[Int] =
+  def updateC2C3a(c1: String, c2: Int, c3: String): Future[Int] =
     db.run(
       (for {
         _ <- testTable.filter(_.c1 === c1).map(_.c2).update(c2)
@@ -120,7 +92,7 @@ class TestData(dbFile: Path) {
       } yield n).transactionally
     )
 
-  def updateC2C3X(c1: String, c2: Int, c3: String) =
+  def updateC2C3b(c1: String, c2: Int, c3: String) =
     db.run(
       DBIO.sequence(Seq(
         testTable.filter(_.c1 === c1).map(_.c2).update(c2),
